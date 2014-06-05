26585b98fa86fdc290f34219639f2dd61fd0ccfb
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2014-06-04 23:37:07 -0500

    Se agrega la gráfica del rendimiento de la aplicación se mandan parametros al programa con el siguiente formato 'python KNNClustering.py [imagen] [cadena_de_numeros_que_contiene_la_imagen]'

--- ProyectoFinal/KNNClustering.py
+++ ProyectoFinal/KNNClustering.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+def contadorLetras(cadena):
+    mapaResultado = [0,0,0,0,0,0,0,0,0,0]
+    for i in xrange(0,len(cadena)):
+        mapaResultado[int(cadena[i])] += 1
+    return mapaResultado
 
 samples = np.loadtxt('generalsamples.data',np.float32)
 responses = np.loadtxt('generalresponses.data',np.float32)
@@ -8,8 +16,16 @@ responses = responses.reshape((responses.size,1))
 
 model = cv2.KNearest()
 model.train(samples,responses)
-
-im = cv2.imread('./Imagenes/test5.png')
+#se cargan los argumentos
+argumentos = sys.argv
+print len(argumentos)
+if len(argumentos)<=1:
+    argumentos = [argumentos[0],'','']
+if len(argumentos[1])>0:
+    imagenPrueba = argumentos[1]
+else:
+    imagenPrueba = './Imagenes/PruebaMini.png'
+im = cv2.imread(imagenPrueba)
 out = np.zeros(im.shape,np.uint8)
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray,(5,5),0)
@@ -26,7 +42,7 @@ contadorAuxiliar = 0
 for cnt in contours:
     if cv2.contourArea(cnt)>50:
         [x,y,w,h] = cv2.boundingRect(cnt)
-        if  h>25:
+        if  h>22:
             cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),1)
             roi = thresh[y:y+h,x:x+w]
             """
@@ -39,7 +55,6 @@ for cnt in contours:
             contadorAuxiliar+=1
 
             raw_input("Continuar ... ")"""
-
             roismall = cv2.resize(roi,(10,10))
             roismall = roismall.reshape((1,100))
             roismall = np.float32(roismall)
@@ -47,9 +62,24 @@ for cnt in contours:
             string = str(int((results[0][0])))
             mapaResultado[int((results[0][0]))] += 1
             cv2.putText(out,string,(x,y+h),0,1,(0,255,0))
-plt.imshow(im)
+#plt.imshow(im)
+#plt.show()
+#print "Cantidad de Elementos en el arreglo \n"
+#for i in xrange(0,10):
+#    print "Cantidad de %d es %d " % (i,mapaResultado[i])
+#fig,ax = plt.figure()
+numeros = ('0','1','2','3','4','5','6','7','8','9')
+y_pos = np.arange(len(numeros))
+
+if len(argumentos[2]) > 0:
+    cantidadOriginal = contadorLetras(argumentos[2])
+    error = []
+    for index in xrange(0,10):
+        valor = float(cantidadOriginal[index]-mapaResultado[index])/cantidadOriginal[index]
+        error.append(valor) 
+else:
+    error = [0.1] * 10
+plt.barh(y_pos, mapaResultado, xerr=error, align='center', alpha=0.4)
+plt.yticks(y_pos,numeros)
+plt.xlabel('Rendimiento')
 plt.show()
-print "Cantidad de Elementos en el arreglo \n"
-for i in xrange(0,10):
-    print "Cantidad de %d es %d " % (i,mapaResultado[i])
-#cv2.waitKey(0)
